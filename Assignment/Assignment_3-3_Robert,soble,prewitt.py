e82daec4b8ccb26c33f7a000d34d865c36a12af6
baba-ti <jjh123456744@gmail.com>
2024-04-08 13:54:50 +0900

    opencv_study

--- Assignment/Assignment_3-3_Robert,soble,prewitt.py
+++ Assignment/Assignment_3-3_Robert,soble,prewitt.py
@@ -27,17 +27,27 @@ sobel_edge_vertical_2 = cv2.filter2D(image_2,-1,sobel_vertical)
 image_1_merged_sobel = np.hstack((image_1_gray, sobel_edge_horizontal + sobel_edge_horizontal ))
 image_2_merged_sobel = np.hstack((image_2, sobel_edge_horizontal_2 + sobel_edge_vertical_2 ))
 
-# prewitt_horizontal = np.array([[-1,-1,-1],[0,1,0],[1,1,1]])
-# prewitt_vertical = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
+prewitt_horizontal = np.array([[-1,-1,-1],[0,1,0],[1,1,1]])
+prewitt_vertical = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
+prewitt_edge_horizontal = cv2.filter2D(image_1_gray,-1,prewitt_horizontal)
+prewitt_edge_vertical = cv2.filter2D(image_1_gray, -1 ,prewitt_vertical)
+prewitt_edge_horizontal_2 = cv2.filter2D(image_2,-1,prewitt_horizontal)
+prewitt_edge_vertical_2 = cv2.filter2D(image_2,-1,prewitt_vertical)
+image_1_merged_prewitt = np.hstack((image_1_gray, prewitt_edge_horizontal + prewitt_edge_vertical))
+image_2_merged_prewitt = np.hstack((image_2, prewitt_edge_horizontal_2 + prewitt_edge_vertical_2))
 
 
 cv2.imshow("Roberts edge ", image_1_gray_merged_robert) 
 cv2.imshow("Roberts edge 2", image_2_merged_robert)
 cv2.waitKey()
+cv2.destroyAllWindows()
 
 cv2.imshow("Sobel edge ", image_1_merged_sobel)
 cv2.imshow("Sobel edge 2", image_2_merged_sobel)
 cv2.waitKey()
-
 cv2.destroyAllWindows()
 
+cv2.imshow("Prewitt edge ", image_1_merged_prewitt)
+cv2.imshow("Prewitt edge 2", image_2_merged_prewitt)
+cv2.waitKey()
+cv2.destroyAllWindows()
