305ae6ae3367e55b740bbceb4639ed53ce16d6de
baba-ti <jjh123456744@gmail.com>
2024-05-03 14:30:20 +0900

    opencv_study

--- Assignment/Assignment_mid_project/avr-str-canny.py
+++ Assignment/Assignment_mid_project/avr-str-canny.py
@@ -1,30 +1,27 @@
+#%%
+
 import cv2
 import numpy as np
 import sys
 import matplotlib.pyplot as plt
 
-
 image_1 = cv2.imread("image/fce(salt_pepper noise).bmp")
 image_2 = cv2.imread("image/Copy of Lena-Gaussian-noise2.jpg")
 image_3 = cv2.imread("image/300px-Kodim17_noisy.jpg")
 
-image_1_gray = cv2.cvtColor(image_1, cv2.COLOR_BGR2GRAY)
-image_2_gray = cv2.cvtColor(image_2, cv2.COLOR_BGR2GRAY)
-image_3_gray = cv2.cvtColor(image_3, cv2.COLOR_BGR2GRAY)
-
-
-cv2.imshow("Original 1", image_1_gray)
-cv2.imshow("Original 2", image_2_gray)
-cv2.imshow("Original 3", image_3_gray)
+cv2.imshow("Original 1", image_1)
+cv2.imshow("Original 2", image_2)
+cv2.imshow("Original 3", image_3)
 
 cv2.waitKey()
 cv2.destroyAllWindows()
 
-# 평균값 필터 3 by 3 avr 필터
+
+# 평균값 필터 3 by 3
 ksize = 3
-image_1_avr = cv2.blur(image_1_gray,(ksize,ksize))
-image_2_avr = cv2.blur(image_2_gray,(ksize,ksize))
-image_3_avr = cv2.blur(image_3_gray,(ksize,ksize))
+image_1_avr = cv2.blur(image_1,(ksize,ksize))
+image_2_avr = cv2.blur(image_2,(ksize,ksize))
+image_3_avr = cv2.blur(image_3,(ksize,ksize))
     
 text = "Mean : {} * {}".format(ksize,ksize)
     
@@ -39,17 +36,16 @@ cv2.imshow("image_3_avr", image_3_avr)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
-#히스토그램 스트레칭
 image_1_avr_str = cv2.normalize(image_1_avr,None,0,255,cv2.NORM_MINMAX) 
 image_2_avr_str = cv2.normalize(image_2_avr,None,0,255,cv2.NORM_MINMAX)
 image_3_avr_str = cv2.normalize(image_3_avr,None,0,255,cv2.NORM_MINMAX)
 
-# hist1 = cv2.calcHist([image_1_avr],[0],None,[256],[0,256])
-# hist2 = cv2.calcHist([image_1_avr_str],[0],None,[256],[0,256])
-# hist3 = cv2.calcHist([image_2_avr],[0],None,[256],[0,256])
-# hist4 = cv2.calcHist([image_2_avr_str],[0],None,[256],[0,256])
-# hist5 = cv2.calcHist([image_3_avr],[0],None,[256],[0,256])
-# hist6 = cv2.calcHist([image_3_avr_str],[0],None,[256],[0,256])
+hist1 = cv2.calcHist([image_1_avr],[0],None,[256],[0,256])
+hist2 = cv2.calcHist([image_1_avr_str],[0],None,[256],[0,256])
+hist3 = cv2.calcHist([image_2_avr],[0],None,[256],[0,256])
+hist4 = cv2.calcHist([image_2_avr_str],[0],None,[256],[0,256])
+hist5 = cv2.calcHist([image_3_avr],[0],None,[256],[0,256])
+hist6 = cv2.calcHist([image_3_avr_str],[0],None,[256],[0,256])
 
 cv2.imshow("image_1_avr_str", image_1_avr_str)
 cv2.imshow("image_2_avr_str", image_2_avr_str)
@@ -73,16 +69,3 @@ cv2.imshow("image_3_avr_str", image_3_avr_str)
 # plt.show()
 cv2.waitKey()
 cv2.destroyAllWindows()
-
-#케니 엣지
-image_1_avr_str_canny = cv2.Canny(image_1_avr_str,100,200)
-image_2_avr_str_canny = cv2.Canny(image_2_avr_str,200,300) 
-image_3_avr_str_canny = cv2.Canny(image_3_avr_str,100,200) 
-
-cv2.imshow("image_1_avr_str_canny",image_1_avr_str_canny)
-cv2.imshow("image_2_avr_str_canny",image_2_avr_str_canny)
-cv2.imshow("image_3_avr_str_canny",image_3_avr_str_canny)
-cv2.waitKey()
-cv2.destroyAllWindows() 
-
-
